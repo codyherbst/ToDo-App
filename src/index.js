c2f189e2d4760240db92d8ff4710c228487e0c4a
Cody Herbst <codyherbst@Codys-MacBook-Pro.local>
2020-03-25 11:22:22 -0400

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ToDoApp from './TodoApp'
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(<ToDoApp />, document.getElementById('root'));
 
